0d97735431ec0274d44b2ead9fbf9197668bdded
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-05-14 08:45:24 -0300

    add max poll records config

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,12 +14,16 @@ public class NewOrderMain {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "123123,123123,12313123";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
-            if(ex != null) {
+        Callback callback = (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
             }
             System.out.println(data.topic() + "::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());
-        }).get();
+        };
+        var email = "Thanks, we are processing you order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
